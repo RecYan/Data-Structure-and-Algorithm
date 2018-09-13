16ea235c8231250071143dc18d9ad13b8dd0049e
Yan <1757901534@qq.com>
2018-09-13 20:36:41 +0800

    测试用例

---
+++ src/BST/bstTest.java
@@ -0,0 +1,30 @@
+package BST;
+
+/**
+ * Created by Yan_Jiang on 2018/9/13.
+ */
+public class bstTest {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<Integer>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+
+        for(int num : nums) {
+            bst.add(num);
+        }
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+
+        bst.preOrder();
+        System.out.println(bst);
+
+
+    }
+}
