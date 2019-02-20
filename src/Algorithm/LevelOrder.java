d4c4a67a679ae0b605b4a9562f58bc20b3445248
Yan <1757901534@qq.com>
2019-02-20 16:14:05 +0800

    二叉树 层序遍历

---
+++ src/Algorithm/LevelOrder.java
@@ -0,0 +1,37 @@
+package Algorithm;
+
+import java.util.LinkedList;
+
+/**
+	二叉树 层序遍历
+		使用队列辅助
+*/
+public class LevelOrder
+{
+  public void levelOrderRecur(Node root)
+  {
+	  if(root == null)
+	  {
+		  return ;
+	  }
+	  LinkedList<Node> queue = new LinkedList<>();
+	  Node current = null;
+	  queue.offer(root);//将根节点入队
+	  while(!queue.isEmpty())
+	  {
+		  current = queue.poll();//出队队头元素并访问
+		  System.out.print(current.val +"-->");
+		  if(current.left != null)//如果当前节点的左节点不为空入队
+		  {
+			  queue.offer(current.left);
+		  }
+		  if(current.right != null)//如果当前节点的右节点不为空，把右节点入队
+		  {
+			  queue.offer(current.right);
+		  }
+	  }
+	  
+  }
+ 
+}
+
